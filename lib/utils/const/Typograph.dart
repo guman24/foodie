106e3670e7c643e731e2db8e4d160edf136a8b92
Rajan Karki <krajantn@gmail.com>
2020-11-05 00:20:06 +0545

    create typograph for nav bar

---
+++ lib/utils/const/Typograph.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+import 'colors.dart';
+
+class Typograph {
+  static final TextStyle tabBarTitle = TextStyle(
+      fontWeight: FontWeight.bold,
+      fontFamily: "robotoBlack",
+      color: ColorsPalette.tealDark);
+
+  static final Color tabBarIconColor =  ColorsPalette.tealDark.withOpacity(0.5);
+}
